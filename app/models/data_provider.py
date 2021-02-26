01a054b8d6352e229b9a3d8fe5af57b8e031c275
achrafBenHamou <achraf.ben130@gmail.com>
2021-02-26 17:39:53 +0100

    last update

--- app/models/data_provider.py
+++ app/models/data_provider.py
@@ -16,20 +16,16 @@ def get_data_from_csv(zip_code,data_path,providers):
             row = list(map(lambda x: int(float(x)), row))
             # zip code is in the last column of the row
             if row[-1] == zip_code:
-                if(providers[row[0]] not in output.keys()):
-                    new_provider_coverage = ProviderCoverage(id = row[0], zip_code= row[-1])
-                    new_provider_coverage.setHas2G(row[3])
-                    new_provider_coverage.setHas3G(row[4])
-                    new_provider_coverage.setHas4G(row[5])
-                    new_provider_coverage.setName(providers[row[0]])
+                # instanciate ProviderCoverage object
+                new_provider_coverage = ProviderCoverage(id = row[0], zip_code= row[-1])
+                new_provider_coverage.setHas2G(row[3])
+                new_provider_coverage.setHas3G(row[4])
+                new_provider_coverage.setHas4G(row[5])
+                new_provider_coverage.setName(providers[row[0]])
+                if (providers[row[0]] not in output.keys()):
                     # add object to the output
                     output[new_provider_coverage.name]= new_provider_coverage
                 else :
-                    new_provider_coverage = ProviderCoverage(id = row[0], zip_code= row[-1])
-                    new_provider_coverage.setHas2G(row[3])
-                    new_provider_coverage.setHas3G(row[4])
-                    new_provider_coverage.setHas4G(row[5])
-                    new_provider_coverage.setName(providers[row[0]])
                     old_provider_coverage = output[new_provider_coverage.name]
                     # takes the newest and the older and return the best network coverage in the city
                     best_provider_coverage = compare_result (new_provider_coverage,old_provider_coverage)
